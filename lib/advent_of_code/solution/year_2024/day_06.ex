0fa978ed0e0e9c3f1ca4f59a27c9708122b2e96e
Adi Purnama <adi.purnama234@gmail.com>
2024-12-06 20:52:10 +0700

    feat(aoc2024): day6

--- lib/advent_of_code/solution/year_2024/day_06.ex
+++ lib/advent_of_code/solution/year_2024/day_06.ex
@@ -1,7 +1,130 @@
 defmodule AdventOfCode.Solution.Year2024.Day06 do
-  def part1(_input) do
+  @left {-1, 0}
+  @right {1, 0}
+  @up {0, -1}
+  @down {0, 1}
+
+  def part1(input) do
+    {map, pos, dir} =
+      input
+      |> to_map_grid()
+
+    {_, coverage} = traverse(map, pos, dir)
+
+    coverage
+    |> Enum.count(fn {_pos, c} ->
+      case c do
+        {"X", _} -> true
+        _ -> false
+      end
+    end)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    {map, starting_pos, dir} =
+      input
+      |> to_map_grid()
+
+    {_, map_coverage} = traverse(map, starting_pos, dir)
+
+    map_coverage
+    |> Enum.filter(fn {pos, val} ->
+      case val do
+        {"X", _} ->
+          pos != starting_pos
+
+        _ ->
+          false
+      end
+    end)
+    |> Enum.map(fn {pos, _val} -> pos end)
+    |> Enum.filter(fn pos ->
+      obs_map = Map.put(map, pos, "O")
+
+      case traverse(obs_map, starting_pos, dir) do
+        {true, _} -> true
+        _ -> false
+      end
+    end)
+    # |> IO.inspect()
+    |> Enum.count()
+  end
+
+  defp traverse(map_grid, pos, dir) do
+    map_grid =
+      case Map.get(map_grid, pos) do
+        {"X", _} -> map_grid
+        _ -> Map.put(map_grid, pos, {"X", dir})
+      end
+
+    new_pos = move(pos, dir)
+
+    case Map.get(map_grid, new_pos) do
+      char when char == "#" or char == "O" ->
+        new_dir = turn(dir)
+        traverse(map_grid, pos, new_dir)
+
+      "." ->
+        traverse(map_grid, new_pos, dir)
+
+      {"X", ^dir} ->
+        {true, map_grid}
+
+      {"X", _} ->
+        traverse(map_grid, new_pos, dir)
+
+      nil ->
+        {false, map_grid}
+    end
+  end
+
+  defp move({x, y}, {dir_x, dir_y}) do
+    {x + dir_x, y + dir_y}
+  end
+
+  defp turn(dir) do
+    case dir do
+      @up ->
+        @right
+
+      @right ->
+        @down
+
+      @down ->
+        @left
+
+      @left ->
+        @up
+    end
+  end
+
+  defp to_map_grid(input) do
+    starting_map =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, row} ->
+        String.graphemes(line)
+        |> Enum.with_index()
+        |> Enum.flat_map(fn {char, col} ->
+          position = {col, row}
+          [{position, char}]
+        end)
+      end)
+      |> Map.new()
+
+    {starting_pos, c} =
+      starting_map
+      |> Enum.find(fn {_pos, c} -> Enum.member?([">", "<", "^", "v"], c) end)
+
+    dir =
+      case c do
+        ">" -> @right
+        "<" -> @left
+        "^" -> @up
+        "v" -> @down
+      end
+
+    {starting_map, starting_pos, dir}
   end
 end
