a3c017f13d4b6e873999e1a89d83e96ece5d6a72
Adi Purnama <adi.purnama234@gmail.com>
2025-01-21 10:00:25 +0700

    feat: aoc 2015 day 03

--- lib/advent_of_code/solution/year_2015/day_03.ex
+++ lib/advent_of_code/solution/year_2015/day_03.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Solution.Year2015.Day03 do
-  def part1(_input) do
+  def part1(input) do
+    init_pos = {0, 0}
+    init_houses = MapSet.new() |> MapSet.put(init_pos)
+
+    String.split(input, "", trim: true)
+    |> Enum.reduce({init_houses, init_pos}, fn ch, {acc, pos} ->
+      new_pos = move(pos, ch)
+      acc = MapSet.put(acc, new_pos)
+
+      {acc, new_pos}
+    end)
+    |> elem(0)
+    |> Enum.count()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    init_pos = {0, 0}
+    init_houses = MapSet.new() |> MapSet.put(init_pos)
+
+    String.split(input, "", trim: true)
+    |> Enum.reduce(
+      {init_houses, init_pos, init_pos, :santa},
+      fn ch, {acc, santa_pos, robo_pos, turn} ->
+        new_pos = if turn == :santa, do: move(santa_pos, ch), else: move(robo_pos, ch)
+        acc = MapSet.put(acc, new_pos)
+
+        if turn == :santa do
+          {acc, new_pos, robo_pos, :robo}
+        else
+          {acc, santa_pos, new_pos, :santa}
+        end
+      end
+    )
+    |> elem(0)
+    |> Enum.count()
+  end
+
+  defp move({x, y}, ch) do
+    case ch do
+      ">" -> {x + 1, y}
+      "<" -> {x - 1, y}
+      "^" -> {x, y + 1}
+      "v" -> {x, y - 1}
+    end
   end
 end
