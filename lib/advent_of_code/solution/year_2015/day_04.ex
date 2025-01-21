a16ca40382112d13f68a81d263c91320ede82225
Adi Purnama <adi.purnama234@gmail.com>
2025-01-21 15:05:38 +0700

    feat: aoc 2015 day 04 05

--- lib/advent_of_code/solution/year_2015/day_04.ex
+++ lib/advent_of_code/solution/year_2015/day_04.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Solution.Year2015.Day04 do
   def part1(_input) do
+    input = "yzbqklnj"
+
+    0..1_000_000
+    |> Enum.find(fn n ->
+      String.starts_with?(md5_hash_string(input <> Integer.to_string(n)), "00000")
+    end)
   end
 
   def part2(_input) do
+    input = "yzbqklnj"
+
+    0..100_000_000
+    |> Enum.find(fn n ->
+      String.starts_with?(md5_hash_string(input <> Integer.to_string(n)), "000000")
+    end)
+  end
+
+  defp md5_hash_string(str) do
+    :crypto.hash(:md5, str) |> Base.encode16(case: :lower)
   end
 end
--- lib/advent_of_code/solution/year_2015/day_05.ex
+++ lib/advent_of_code/solution/year_2015/day_05.ex
@@ -1,7 +1,66 @@
 defmodule AdventOfCode.Solution.Year2015.Day05 do
-  def part1(_input) do
+  defmodule Part1 do
+    # Three or more vowels
+    @vowels_regex ~r/[aeiou].*[aeiou].*[aeiou]/
+
+    # At least one letter appearing twice in a row
+    @double_letter_regex ~r/(.)\1/
+
+    # Forbidden strings
+    @forbidden_pairs_regex ~r/ab|cd|pq|xy/
+
+    def nice_string?(str) do
+      has_three_vowels?(str) and
+        has_double_letter?(str) and
+        not has_forbidden_pairs?(str)
+    end
+
+    # Helper functions for each rule
+    defp has_three_vowels?(str) do
+      String.match?(str, @vowels_regex)
+    end
+
+    defp has_double_letter?(str) do
+      String.match?(str, @double_letter_regex)
+    end
+
+    defp has_forbidden_pairs?(str) do
+      String.match?(str, @forbidden_pairs_regex)
+    end
   end
 
-  def part2(_input) do
+  defmodule Part2 do
+    # Pair of letters that appears twice without overlapping
+    @repeating_pair_regex ~r/(..).*\1/
+
+    # Letter that repeats with exactly one letter between
+    @letter_sandwich_regex ~r/(.).\1/
+
+    def nice_string?(str) do
+      has_repeating_pair?(str) and has_letter_sandwich?(str)
+    end
+
+    defp has_repeating_pair?(str) do
+      String.match?(str, @repeating_pair_regex)
+    end
+
+    defp has_letter_sandwich?(str) do
+      String.match?(str, @letter_sandwich_regex)
+    end
+  end
+
+  def part1(input) do
+    nice_string_count(input, &Part1.nice_string?/1)
+  end
+
+  def part2(input) do
+    nice_string_count(input, &Part2.nice_string?/1)
+  end
+
+  defp nice_string_count(input, evaluator) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.filter(evaluator)
+    |> Enum.count()
   end
 end
